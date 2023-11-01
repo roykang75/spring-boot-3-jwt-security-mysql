0b2edb0cd16b2a8c3f8c3bce234b0e8645c89a46
sungtae0314.kang <sungtae0314.kang@lgepartner.com>
2023-11-01 16:48:36 +0900

    잘못된 코드 수정

--- src/main/java/com/alibou/security/book/BookController.java
+++ src/main/java/com/alibou/security/book/BookController.java
@@ -1,7 +1,7 @@
 package com.alibou.security.book;
 
 import com.alibou.security.common.service.ResponseService;
-import com.alibou.security.common.service.response.SingleResult;
+import com.alibou.security.common.service.response.ListResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -34,7 +32,7 @@ public class BookController {
 //    }
 
     @GetMapping
-    public SingleResult<List<Book>> findAllBooks() {
-        return responseService.getSingleResult(service.findAll());
+    public ListResult<Book> findAllBooks() {
+        return responseService.getListResult(service.findAll());
     }
 }
